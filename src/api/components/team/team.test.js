cca05ca2385d2583ebc325f7d85040d1a4fe5423
Ken Holston(Work) <git.kholston@gmail.com>
2021-01-12 04:52:10 +0000

    import supertest

--- src/api/components/team/team.test.js
+++ src/api/components/team/team.test.js
@@ -1,3 +1,8 @@
+import supertest from 'supertest';
+import app from '../../server';
+
+const request = supertest(app);
+
 describe('Initial Jest Tests', () => {
   test('Testing Jest in: Team', () => {
     expect(1).toBe(1);
--- src/api/components/user/user.test.js
+++ src/api/components/user/user.test.js
@@ -1,3 +1,8 @@
+import supertest from 'supertest';
+import app from '../../server';
+
+const request = supertest(app);
+
 describe('Initial Jest Tests', () => {
   test('Testing Jest in: User', () => {
     expect(1).toBe(1);
