4cb3f0a300be430b98ac2444e3af35c1cc8ad16f
Ken Holston <git.kholston@gmail.com>
2021-01-12 05:01:09 -0500

    create project model

---
+++ src/api/components/project/project.model.js
@@ -0,0 +1,26 @@
+import mongoose, { SchemaType, Types } from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const ProjectSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    team: { type: Schema.Types.ObjectId, ref: 'Team', required: true },
+    tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }],
+    status: {
+      type: String,
+      enum: ['Planning', 'In Progress', 'Completed', 'Archived'],
+      default: 'Planning',
+    },
+  },
+  { timestamps: true },
+);
+
+ProjectSchema.pre('remove', (next) => {
+  this.model('Tasks').deleteMany({ project: this.id }, next);
+});
+
+const Project = mongoose.model('Project', ProjectSchema);
+export default Project;
