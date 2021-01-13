1742845511c0dbe7158d0e47888af3c2eef6a7c4
Ken Holston(Work) <git.kholston@gmail.com>
2021-01-13 20:53:04 +0000

    update user test skeletons

--- src/api/components/user/user.test.js
+++ src/api/components/user/user.test.js
@@ -1,10 +1,41 @@
 import app from '../../app';
+import { connectDB, closeDB, clearDB } from '../../config/mongoConfigTesting';
 import supertest from 'supertest';
 
 const request = supertest(app);
 
 describe('Initial Jest Tests', () => {
-  test('Testing Jest in: User', () => {
+  it('Testing Jest in: User', () => {
+    expect(1).toBe(1);
+  });
+});
+
+beforeAll(async () => {
+  await connectDB();
+});
+
+afterAll(async () => {
+  await closeDB();
+});
+
+describe('Testing User Routes', () => {
+  it('POST: Create a user', () => {
+    expect(1).toBe(1);
+  });
+
+  it('GET: Read a user', () => {
+    expect(1).toBe(1);
+  });
+
+  it('GET: Read all users', () => {
+    expect(1).toBe(1);
+  });
+
+  it('POST: Update a user', () => {
+    expect(1).toBe(1);
+  });
+
+  it('DELETE: Remove a user', () => {
     expect(1).toBe(1);
   });
 });
