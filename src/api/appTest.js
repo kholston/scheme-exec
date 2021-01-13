90664ab4037a40723191a9ee334499c47dea299f
Ken Holston(Work) <git.kholston@gmail.com>
2021-01-13 16:07:10 +0000

    create test version of app

---
+++ src/api/appTest.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+const app = express();
+
+// Setup Middleware
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Setup routes
+
+export default app;
