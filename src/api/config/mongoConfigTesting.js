eda5500ebebf51dedea28e901273e0150705eb64
Ken Holston(Work) <git.kholston@gmail.com>
2021-01-13 16:08:12 +0000

    restructure mongodb test config

--- src/api/config/mongoConfigTesting.js
+++ src/api/config/mongoConfigTesting.js
@@ -5,24 +5,31 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 const log = debug('schemeExec:mongodb-test');
 const mongoServer = new MongoMemoryServer();
 
-//mongoose.Promise = Promise;
-mongoServer.getConnectionString().then((mongoURI) => {
+export const connectDatabase = async ()=>{
+  const uri = await mongoServer.getConnectionString();
+  
   const mongooseOpts = {
+    useNewUrlParser: true,
     autoReconnect: true,
     reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 1000,
-    useNewUrlParser: true,
+    reconnectInterval:1000,
   };
+  
+  await mongoose.connect(uri,mongooseOpts);
+}
 
-  mongoose.connect(mongoURI, mongooseOpts);
-  mongoose.connection.on('error', (e) => {
-    if (e.message.code === 'ETIMEDOUT') {
-      log(e);
-      mongoose.connect(mongoURI, mongooseOpts);
-    }
-    log(e);
-  });
-  mongoose.connection.once('open', () => {
-    log(`MongoDB successfully connected to ${mongoURI}`);
-  });
-});
+export const closeDatabase = async()=>{
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongoServer.stop();
+}
+
+export const clearDatabase = async ()=>{
+  const collections = mongoose.connection.collections;
+  
+  for(const key in collections){
+    const collection = collections[key];
+    await collection.deleteMany();
+  };
+}
+
