3125ce59432d90ad61e5193c1b8455615f5d2b1e
Ken <kennyk3n@live.com>
2021-01-15 01:39:42 -0500

    add routes to app

--- src/api/app.js
+++ src/api/app.js
@@ -2,6 +2,10 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import projectRouter from './components/project/project.routes';
+import taskRouter from './components/task/task.routes';
+import teamRouter from './components/team/team.routes';
+import userRouter from './components/user/user.routes';
 
 const app = express();
 
@@ -12,5 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Setup routes
+app.use('/projects', projectRouter);
+app.use('/tasks', taskRouter);
+app.use('/teams', teamRouter);
+app.use('/users', userRouter);
 
 export default app;
